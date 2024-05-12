7166080e3db6c0f773f4b25b9e4ac61906e76344
Aditya Dey <deyaditya2004@gmail.com>
2024-05-12 13:05:07 +0530

    add app.js including important js topic

---
+++ app.js
@@ -0,0 +1,19 @@
+alert('HELLO JAVASCRIPT');
+
+console.log("HELLO JAVASCRIPT")
+
+
+// JS Statements
+console.log("MERN")
+
+document.getElementById('title').innerHTML = "DSA";
+
+let totalScore = 500 + 320;
+console.log(totalScore);
+
+if (totalScore > 800) {
+    console.log("GOOD SCORE")
+}
+else {
+    console.log("NOT GOOD")
+}
--- js1.js
+++
@@ -1,3 +0,0 @@
-//this is first file of js1 repository
-
-//this is second line {added on 12/05/2024}
