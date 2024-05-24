57f9f22416a03a454195a0e96eb5f4fac9b99363
Aditya Dey <deyaditya2004@gmail.com>
2024-05-24 13:34:09 +0530

    add Conditional Statements

---
+++ Conditional Statements/1_question.js
@@ -0,0 +1,23 @@
+/*
+if(){
+
+}else if(){
+
+}else{
+
+}
+
+*/
+
+/*
+switch(){
+    case "":
+        conole.log()
+        break
+    case "":
+        console.log()
+        break
+    default:
+        console,log()
+}
+*/
---
+++ Conditional Statements/Conditional_Statements.js
@@ -0,0 +1,40 @@
+// Conditional Statements
+
+/*
+    if
+    else
+    else if
+*/
+
+/*
+    switch
+*/
+
+
+const score = 0
+
+if (score > 0) {
+    console.log("Score is Positive")
+}else if (score < 0) {
+    console.log("Score is Not Positive")
+}else {
+    console.log("Score is ZERO")
+}
+
+
+
+const color = "red"
+
+switch(color) {
+    case "red":
+        console.log("color is red")
+        break
+    case "blue":
+        console.log("color is blue")
+        break
+    case "green":
+        console.log("color is green")
+        break
+    default:
+        console.log("not a valid color")
+}
