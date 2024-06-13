0cef48c23c064bf338311729aa43e3c9f70a75ef
Aditya Dey <deyaditya2004@gmail.com>
2024-06-13 12:57:01 +0530

    add 1 & 2 question

--- Data Types/1_question.js
+++ Data Types/1_question.js
@@ -1,1 +1,7 @@
-// HELLO Q1
+// Store Phone Number. It's vlaue can't be changed.
+
+const phoneNumber = 9500000001;
+
+    // phoneNumber = 6500000002; (not possible beacue of const)
+
+console.log(phoneNumber);
--- Data Types/2_question.js
+++ Data Types/2_question.js
@@ -1,1 +1,31 @@
-//HELLO Q2
+// Use typeof operator and apply on multiple variables.
+
+// Strings
+const userName = "First User";
+console.log(userName);
+console.log(typeof(userName));
+
+
+// Numbers
+const userCode = 1;
+console.log(userCode);
+console.log(typeof(userCode));
+
+
+// Booleans
+const isUserLoggedIn = true;
+console.log(isUserLoggedIn);
+console.log(typeof(isUserLoggedIn));
+
+
+// Object
+const userDetails = {firstName: "First", lastName: "User"};
+console.log(userDetails);
+console.log(typeof(userDetails));
+
+
+
+// Array Object
+const papers = ["DSA", "DAA", "c", "OS", "COMPUTER NETWORKS", "OOPS", "DBMS"];
+console.log(papers);
+console.log(typeof(papers));
