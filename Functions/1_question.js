e7a7231d776c944d53b56c7c639e25acb0777fd8
Aditya Dey <deyaditya2004@gmail.com>
2024-06-17 13:06:22 +0530

    complete 1_question of function

--- Functions/1_question.js
+++ Functions/1_question.js
@@ -1,1 +1,38 @@
-// Functions
+/*
+    Write a JavaScript function that reverses a number.
+        Sample Data and output:
+        Example number = 565758
+        Expected Output: 857565
+*/
+
+
+function reverseNumber(number) {
+    
+    number = number.toString();
+
+    return number.split(``).reverse().join(``);
+}
+
+console.log(Number(reverseNumber(565758)));
+
+
+
+
+
+
+
+/*
+    Write a JavaScript function that reverses a number.
+        Sample Data and output:
+        Example number = 987665
+        Expected Output: 566789
+*/
+
+function reverseNum(anotherNumber) {
+
+    anotherNumber = anotherNumber + ``;
+
+    return anotherNumber.split(``).reverse().join(``);
+
+}
+console.log(Number(reverseNum(987665)));
