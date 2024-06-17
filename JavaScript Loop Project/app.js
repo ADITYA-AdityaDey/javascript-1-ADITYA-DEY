264f22d1d459e31e29cc314d456eae4deff17421
Aditya Dey <deyaditya2004@gmail.com>
2024-06-17 13:44:34 +0530

    create case

--- JavaScript Loop Project/app.js
+++ JavaScript Loop Project/app.js
@@ -1,3 +1,38 @@
+// Guess The Number Function
+function guessTheNumber(userInputDifficulty) {
+    let randomNumber;
+    let attempts = 0;
+    let maximumAttemts;
+
+    // Set The Difficulty Parameters
+    switch (userInputDifficulty){
+        case `easy`:
+            maximumAttemts = 5;
+            randomNumber = Math.floor(Math.random() *10) + 1; // range 1-10
+            // console.log(randomNumber);
+            break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Get User Input Difficulty
 
-let userInputDifficulty = prompt(`Choose Difficulty: (easy, medium, hard)`);
+let userInputDifficulty = prompt(`Choose Difficulty: (easy, medium, hard)`).toLowerCase();
+guessTheNumber(userInputDifficulty);
+//console.log(userInputDifficulty);
+
