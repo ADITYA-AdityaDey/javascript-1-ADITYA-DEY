0584db04d38e6ca8d795f3a12b33ab96e0798380
Aditya Dey <deyaditya2004@gmail.com>
2024-06-21 12:46:42 +0530

    add Encapsulation

--- OOP/app.js
+++ OOP/app.js
@@ -2,19 +2,23 @@ class Person {
     #user1FirstName; // Private Access Modifier
     _user1LastName; // Protected Access Modifier
     user1Code; // Public Access Modifier
-    user1Email; // Public Access Modifier
+    #user1Email; // Private Access Modifier
     User1Country; // Public Access Modifier 
 
     constructor(u1fn, u1ln, u1c, u1e, u1coun) {
         this.#user1FirstName = u1fn;
         this._user1LastName = u1ln;
         this.user1Code = u1c;
-        this.user1Email = u1e;
+        this.#user1Email = u1e;
         this.User1Country = u1coun;
     }
 
     user1FullName() {
         return this.#user1FirstName + ` ` + this._user1LastName;
+    }
+
+    userEmaiID(){
+        console.log(this.#user1Email);
     }
 
 }
@@ -25,4 +29,6 @@ let obj = new Person(`First`, `User`, 1, `abcxyz@email.com`, `India`);
 
 console.log(obj._user1LastName);
 
-console.log(obj.user1Email, obj.User1Country);
+console.log(obj.User1Country);
+
+obj.userEmaiID();
