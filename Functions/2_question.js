7c22269f8db3f807cc96d0e7e36ca4a16fcfc887
Aditya Dey <deyaditya2004@gmail.com>
2024-06-23 23:11:38 +0530

    Modify 2_question.js of Functions

--- Functions/2_question.js
+++ Functions/2_question.js
@@ -1,1 +1,38 @@
-// q2
+// Check Even or Odd
+
+function isEvenorOdd(numberForEvenOdd){
+    return numberForEvenOdd % 2 === 0;
+}
+
+console.log(isEvenorOdd(98));
+console.log(isEvenorOdd(99));
+
+
+
+// Factorial of a Number
+function facto(numberForFactorial){
+    if (numberForFactorial === 0){
+        return 1;
+    }
+    return numberForFactorial * facto(numberForFactorial - 1);
+}
+
+console.log(facto(9));
+
+
+
+// Convert Celsius to Fahrenheit
+
+function celToFahFun(celsiusValue){
+    return (celsiusValue * 9 / 5) + 32;
+}
+
+console.log(celToFahFun(32));
+
+
+
+// Find Maximum number in an array
+function findMaxFun(arrayOfNumbers){
+    return Math.max(...arrayOfNumbers);
+}
+console.log(findMaxFun([27, 43, 34, 45, 8, 90, 100, 200]));
