41a1011748696fb62e4875060905844263c953e9
Aditya Dey <deyaditya2004@gmail.com>
2024-06-24 13:33:08 +0530

    Modified JavaScript Loop Project app.js File

--- JavaScript Loop Project/app.js
+++ JavaScript Loop Project/app.js
@@ -11,21 +11,21 @@ function guessTheNumber(userInputDifficulty) {
             rangeLimit = 10;
             maximumAttemts = 5;
             randomNumber = Math.floor(Math.random() *10) + 1; // range 1-10
-            // console.log(randomNumber);
+            console.log(randomNumber);
             break;
         
         case `medium`:
             rangeLimit = 100;
             maximumAttemts = 7;
             randomNumber = Math.floor(Math.random() *100) + 1; // range 1-100
-            // console.log(randomNumber);
+            console.log(randomNumber);
             break;
         
         case `hard`:
             rangeLimit = 1000;
             maximumAttemts = 10;
             randomNumber = Math.floor(Math.random() *1000) + 1; // range 1-1000
-            // console.log(randomNumber);
+            console.log(randomNumber);
             break;
         default:
             console.error(`Invalid Difficulty Level choose "easy", "medium", "hard"`);
@@ -34,9 +34,15 @@ function guessTheNumber(userInputDifficulty) {
 
     // Input your Number
     while (attempts < maximumAttemts){
-        let userInputGuessValue = parseInt(prompt(`Guess a Number between 1 to ${rangeLimit}`));
+        let userInputGuessValue = parseInt(prompt(`Guess a Number between 1 to ${rangeLimit}. You have ${maximumAttemts - attempts} Attempts.`));
         attempts++;
-        console.log(userInputGuessValue)
+
+        if (userInputGuessValue === randomNumber){
+            console.log(`Congratulations You Guessed the Number in`, attempts, `Attempts.`);
+        }
+
+
+        console.log(userInputGuessValue);
     }
 };
 
@@ -61,4 +67,3 @@ function guessTheNumber(userInputDifficulty) {
 let userInputDifficulty = prompt(`Choose Difficulty: (easy, medium, hard)`).toLowerCase();
 guessTheNumber(userInputDifficulty);
 //console.log(userInputDifficulty);
-
