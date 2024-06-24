b69926473bdb70b67e2290b3d8ba71d1951d3cee
Aditya Dey <deyaditya2004@gmail.com>
2024-06-24 12:13:29 +0530

    Modify Loop Project JavaScript

--- JavaScript Loop Project/app.js
+++ JavaScript Loop Project/app.js
@@ -3,22 +3,26 @@ function guessTheNumber(userInputDifficulty) {
     let randomNumber;
     let attempts = 0;
     let maximumAttemts;
+    let rangeLimit;
 
     // Set The Difficulty Parameters
     switch (userInputDifficulty){
         case `easy`:
+            rangeLimit = 10;
             maximumAttemts = 5;
             randomNumber = Math.floor(Math.random() *10) + 1; // range 1-10
             // console.log(randomNumber);
             break;
         
         case `medium`:
+            rangeLimit = 100;
             maximumAttemts = 7;
             randomNumber = Math.floor(Math.random() *100) + 1; // range 1-100
             // console.log(randomNumber);
             break;
         
         case `hard`:
+            rangeLimit = 1000;
             maximumAttemts = 10;
             randomNumber = Math.floor(Math.random() *1000) + 1; // range 1-1000
             // console.log(randomNumber);
@@ -26,6 +30,14 @@ function guessTheNumber(userInputDifficulty) {
         default:
             console.error(`Invalid Difficulty Level choose "easy", "medium", "hard"`);
     };
+
+
+    // Input your Number
+    while (attempts < maximumAttemts){
+        let userInputGuessValue = parseInt(prompt(`Guess a Number between 1 to ${rangeLimit}`));
+        attempts++;
+        console.log(userInputGuessValue)
+    }
 };
 
 
