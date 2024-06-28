0b1e00cd5587a49c1994c630c94476133a78ad2a
Aditya Dey <deyaditya2004@gmail.com>
2024-06-28 21:18:51 +0530

    modified

--- OOP/app.js
+++ OOP/app.js
@@ -1,45 +1,42 @@
-class Person{
+class Person {
     #firstName;
     lastName;
     _age;
-    constructor(fn, ln, ag){
+    constructor(fn, ln, ag) {
         this.#firstName = fn;
         this.lastName = ln;
         this._age = ag;
     }
 
-    fullName(){
-        console.log(this.#firstName, this.lastName);
+    fullName() {
+        return this.#firstName + ' ' + this.lastName;
     }
 
-    ageFun(){
+    ageFun() {
         console.log(this._age);
     }
 }
 
-class Teacher extends Person{
+class Teacher extends Person {
     subject;
     constructor(fn, ln, ag, sub) {
         super(fn, ln, ag);
         this.subject = sub;
     }
-    display(){
-        return this.lastName + ` ` + this.subject + ` ` + this._age;
+    display() {
+        return this.lastName + ' ' + this.subject + ' ' + this._age;
+    }
+
+    fullName() {
+        return `override`;
     }
 }
 
-
-
-
-
 let objPersonClass = new Person(`User`, `One`, 20);
-objPersonClass.fullName();
+console.log(objPersonClass.fullName());
 objPersonClass.ageFun();
 
-
 let objTeacherClass = new Teacher(`Teacher`, `One`, 30, `JavaScript`);
-objTeacherClass.fullName();
+console.log(objTeacherClass.fullName());
 console.log(objTeacherClass.display());
-
-
-
+console.log(objTeacherClass.fullName());
