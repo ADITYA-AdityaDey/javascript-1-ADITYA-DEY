e6188ae2e75ddcfff1e3fbeecfe76558cbd668c2
Aditya Dey <deyaditya2004@gmail.com>
2024-06-28 21:33:21 +0530

    modified

--- Arrays/1_question.js
+++ Arrays/1_question.js
@@ -1,1 +1,20 @@
-// Arrays
+// Find Maximum Element
+
+function maxElement(array){
+    return Math.max(...array);
+}
+
+elements = [23, 64, 84, 90];
+console.log(maxElement(elements));
+
+
+
+// Reverse an Array
+
+function reverseArrFun(array){
+    return array.slice().reverse();
+}
+
+elementsofArray = [`J`, `a`, `v`, `a`, `S`, `c`, `r`, `i`, `p`, `t`];
+
+console.log(reverseArrFun(elementsofArray));
