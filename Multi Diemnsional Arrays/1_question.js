563a66fc8d660728c078ddc25086ac44d81c89f7
Aditya Dey <deyaditya2004@gmail.com>
2024-06-30 18:00:49 +0530

    modified multidimensional array question 1

--- Multi Diemnsional Arrays/1_question.js
+++ Multi Diemnsional Arrays/1_question.js
@@ -1,1 +1,15 @@
-// Multi-Dimentional Array
+// Accessing Elements in a 2D Array
+
+const arrayMultiD = [
+    [`Employee 1`, `Employee 2`, `Employee 3`],
+    [1, 2, 3],
+    [`xyz@email.com`, `abc@email.com`, `xay@email.com`]
+];
+
+console.log(arrayMultiD[0][2]);
+console.log(arrayMultiD[1][1]);
+console.log(arrayMultiD[2][0]);
+
+
+
+// 
