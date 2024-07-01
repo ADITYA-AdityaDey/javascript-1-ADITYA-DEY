26dec26bcb2947d70f707b444571f8d74c842eee
Aditya Dey <deyaditya2004@gmail.com>
2024-07-01 20:30:30 +0530

    modified

--- Multi Diemnsional Arrays/1_question.js
+++ Multi Diemnsional Arrays/1_question.js
@@ -12,4 +12,45 @@ console.log(arrayMultiD[2][0]);
 
 
 
-// 
+// Modifying Elements in a 2D Array
+
+let matrix = [
+    [10, 20, 30],
+    [40, 50, 60],
+    [70, 80, 90]
+];
+
+matrix[0][2] = 25;
+
+console.log(matrix);
+
+
+
+// Looping Through a 2D Array
+
+let anotherMatrix = [
+    [100, 90, 80],
+    [70, 60, 50],
+    [40, 30, 20]
+];
+
+for (let i = 0; i < anotherMatrix.length; i++) {
+    for (let j = 0; j < anotherMatrix[i].length; j++) {
+        console.log(anotherMatrix[i][j]);
+    }
+}
+
+
+
+let newMat = [
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10],
+    [11, 12, 13, 14, 15],
+    [16, 17, 18, 19, 20]
+];
+
+for (let i = 0; i < newMat.length; i++){
+    for (let j = 0; j < newMat[i].length; j++){
+        console.log(newMat[i][j]);
+    }
+}
