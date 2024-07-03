dd3fda337bf0b5a3e28426c75ba0f62df16addb3
Aditya Dey <deyaditya2004@gmail.com>
2024-07-03 18:05:30 +0530

    modified question 2

--- Multi Diemnsional Arrays/2_question.js
+++ Multi Diemnsional Arrays/2_question.js
@@ -1,3 +1,18 @@
-/*
-    Multi-Dimensional Arrays
-*/
+// create a 3D array
+
+let threeDArray = [
+    [
+    [10, 20, 30],
+    [40, 50, 60]
+    ],
+    [
+        [70, 80, 90],
+        [100, 110, 120]
+    ]
+
+];
+
+
+console.log(threeDArray);
+
+console.log(threeDArray[0][1][0]);
