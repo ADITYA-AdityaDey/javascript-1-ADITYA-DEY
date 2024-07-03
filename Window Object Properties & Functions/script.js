ee3271c37de7172fdcdd11118af9cfbcf24e3afe
Aditya Dey <deyaditya2004@gmail.com>
2024-07-03 17:55:11 +0530

    Create Window object properties & Functions

---
+++ Window Object Properties & Functions/script.js
@@ -0,0 +1,10 @@
+console.log(`Manipulating web pages using JavaScript`);
+
+// Window object properties
+
+let a = document;
+a = window.innerWidth;
+a = innerHeight;
+a = location.toString();
+
+console.log(a);
