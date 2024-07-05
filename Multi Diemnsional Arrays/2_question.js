e80c003c61f4a7ea60c721885d88da3013adf7a0
Aditya Dey <deyaditya2004@gmail.com>
2024-07-05 11:21:19 +0530

    modified

--- Multi Diemnsional Arrays/2_question.js
+++ Multi Diemnsional Arrays/2_question.js
@@ -16,3 +16,21 @@ let threeDArray = [
 console.log(threeDArray);
 
 console.log(threeDArray[0][1][0]);
+
+
+
+// Sum of Elements in 2D Array
+
+let matrix = [
+    [10, 20, 30],
+    [40, 50, 60]
+];
+
+let sum = 0;
+for (let i = 0; i < matrix.length; i++){
+    for (let j = 0; j < matrix.length; j++) {
+        sum += matrix[i][j];
+    }
+}
+
+console.log(`Sum of Elemnets in 2D Arrya: `, sum);
