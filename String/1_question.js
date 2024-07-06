76e53fe9bde7d320644e727e2e80fc8bbd9aebc1
Aditya Dey <deyaditya2004@gmail.com>
2024-07-06 11:36:11 +0530

    add 1_question in Strings

--- String/1_question.js
+++ String/1_question.js
@@ -1,2 +1,32 @@
-// JavaScript String
-// 1_question
+// Reverse String
+
+function reverseSringFun(StringForReverse) {
+    return StringForReverse.split(``).reverse().join(``);
+}
+
+let reversedString = reverseSringFun(`JavaScript Strings`);
+
+console.log(reversedString);
+
+
+
+// Check if a String is a Palindrome
+
+function palindromeFun(stringForPalindromeCheck) {
+    let reversedString = stringForPalindromeCheck.split('').reverse().join('');
+    return stringForPalindromeCheck === reversedString;
+}
+console.log(palindromeFun(`madam`));
+console.log(palindromeFun(`JavaScript`));
+
+
+
+// Find the Longest Word in a Sentence
+
+function longestWordFun(SentenceForLongestWordChck) {
+    let words = SentenceForLongestWordChck.split(' ');
+    let Longest = words.reduce((a, b) => a.length > b.length ? a:b);
+    return Longest;
+}
+
+console.log(`Longest Word is: `,longestWordFun(`Data Structures & Algorithms`));
