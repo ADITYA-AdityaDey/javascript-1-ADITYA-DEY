71020e89f07c3b973c176733e2d7753eb7862fa5
Aditya Dey <deyaditya2004@gmail.com>
2024-07-07 11:27:52 +0530

    modified 2_question String

--- String/2_question.js
+++ String/2_question.js
@@ -1,2 +1,7 @@
-// JavaScript String
-// 2_question
+// Convert the First Letter of Each Word to Uppercase
+
+function capitalizeFirstLetterFun(stringForCapitalize) {
+    return stringForCapitalize.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+}
+
+console.log(capitalizeFirstLetterFun(`java script`));
